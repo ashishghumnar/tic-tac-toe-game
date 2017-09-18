b41c4a05fb87b494df04aa29c1b258f27552ad56
aghumnar <ashish_ghumnar_tp@bmc.com>
2017-09-18 17:15:42 +0530

    change package.json

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express'),
 	path = require('path');
 	app = express();
-	
+
+var server = require('http').Server(app);
+
 	app.use(express.static(path.join(__dirname, 'client')));
 	
-	app.listen(3000, function(){
+	
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 9004,
+    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || 'localhost';
+	
+	server.listen(port, ip, function(){
 		console.log('server started on 3000');
 	});
