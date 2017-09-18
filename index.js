fdc2b6e5792048ce6f07c151a852c7bc79dc0e3b
aghumnar <ashish_ghumnar_tp@bmc.com>
2017-09-18 17:30:34 +0530

    change in  index.js

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ var server = require('http').Server(app);
 	app.use(express.static(path.join(__dirname, 'client')));
 	
 	
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 9004,
-    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || 'localhost';
-	console.log(port, ip);
-	
-	server.listen(port, ip);
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+ console.log(server_port, server_ip_address);
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
